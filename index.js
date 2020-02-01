5fb5f7af84e061c6da8d15e7560b02ef8ee77005
Aman-malhotra <official.aman18@gmail.com>
2020-02-01 22:51:39 +0530

    new users

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var socket = require('socket.io');
+const { Firestore } = require('@google-cloud/firestore');
+
+// Create a new client
+const firestore = new Firestore();
 
 // app start
 
@@ -15,13 +19,14 @@ app.use(express.static('public'));
 //socket setup
 var io = socket(server);
 
-io.on('connection', (socket) =>{
-    console.log("made socket connection", socket.id);
-    socket.on('chat', (data)=>{
+io.on('connection', (socket) => {
+    console.log("made socket connection", socket.id + " data" + JSON.stringify(socket.request._query));
+    socket.on('chat', (data) => {
         io.sockets.emit('chat', data);
     });
 
-    socket.on('typing', (data)=>{
+
+    socket.on('typing', (data) => {
         socket.broadcast.emit(
             'typing', data
         );
--- public/chat.js
+++ public/chat.js
@@ -1,21 +1,30 @@
 // Make connection
 var socket = io.connect('http://localhost:4000');
 
+var socket;
 
 // Query DOM
 var message = document.getElementById('message');
 var handle = document.getElementById('handle');
-var btn = document.getElementById('send');
+var group_id = document.getElementById('group_id');
+var group_title = document.getElementById('group_title');
+var name_group = document.getElementById('name_group');
+var send_btn = document.getElementById('send');
 var output = document.getElementById('output');
 var feedback = document.getElementById('feedback');
 
 // emit Event
-btn.addEventListener('click', () => {
-    console.log('sending data to backend')
+send_btn.addEventListener('click', () => {
     socket.emit('chat', {
         message: message.value,
-        handle: handle.value
-    })
+        handle: handle.value,
+        group_id: group_id.value
+    });
+});
+
+name_group.addEventListener('click', () => {
+    group_title.innerHTML = group_id.value
+    group_id.disabled = true;
 });
 
 message.addEventListener('keypress', () => {
@@ -25,10 +34,16 @@ message.addEventListener('keypress', () => {
 
 // listen for events
 socket.on('chat', (data) => {
-    feedback.innerHTML = "";
-    output.innerHTML += '<p><strong>' + data.handle + '</strong> ' + data.message + '</p>';
+    if (data.group_id == group_id.value) {
+        feedback.innerHTML = "";
+        output.innerHTML += '<p><strong>' + data.handle + '</strong> ' + data.message + '</p>';
+    }
+
 });
 
 socket.on('typing', (data) => {
-    feedback.innerHTML = '<p><strong>' + data + '</strong>' + " is typing" + '</p>';
+    if (data.group_id == group_id.value) {
+        feedback.innerHTML = '<p><strong>' + data + '</strong>' + " is typing" + '</p>';
+    }
+
 }); 
